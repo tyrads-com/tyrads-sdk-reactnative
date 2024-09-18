70546004a2d6c7672f09d9d7266315b0f2148a52
ibnShamas <ibnShamas@gmail.com>
2024-09-18 08:27:40 +0530

    chore: fixed campaign id issue

--- ios/Tyrads/Tyrads.swift
+++ ios/Tyrads/Tyrads.swift
@@ -144,7 +144,7 @@ public class Tyrads {
      public func showOffers(_ launchMode: Int = 3, route: String? = nil, campaignID: Int? = nil) {
         self.initializationWait.wait()
         var urlString =
-        "https://websdk.tyrads.com/?apiKey=\(Tyrads.instance.apiKey)&apiSecret=\(Tyrads.instance.apiSecret)&userID=\(Tyrads.instance.publisherUserID)&newUser=\(Tyrads.instance.newUser)&platform=\(AcmoConfig.SDK_PLATFORM)&hc=\(Tyrads.instance.loginData?.data.publisherApp.headerColor ?? "")&mc=\(Tyrads.instance.loginData?.data.publisherApp.mainColor ?? "")&launchMode=\(launchMode)&route=\(route ?? "")&campaignID=\(campaignID ?? "")"
+        "https://websdk.tyrads.com/?apiKey=\(Tyrads.instance.apiKey)&apiSecret=\(Tyrads.instance.apiSecret)&userID=\(Tyrads.instance.publisherUserID)&newUser=\(Tyrads.instance.newUser)&platform=\(AcmoConfig.SDK_PLATFORM)&hc=\(Tyrads.instance.loginData?.data.publisherApp.headerColor ?? "")&mc=\(Tyrads.instance.loginData?.data.publisherApp.mainColor ?? "")&launchMode=\(launchMode)&route=\(route ?? "")&campaignID=\(campaignID)"
 
 
         if let url = URL(string: urlString) {
