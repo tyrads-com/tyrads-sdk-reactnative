3ae06f75e876e68444b6fa861d513c687503d2c6
basharatmehdi <basharatmehdidar@gmail.com>
2025-04-15 13:33:13 +0530

    ios changes in bridging

--- ios/TyradsSdk.mm
+++ ios/TyradsSdk.mm
@@ -3,7 +3,10 @@
 @interface RCT_EXTERN_MODULE(TyradsSdk, NSObject)
 
 RCT_EXTERN_METHOD(init:(NSString *)apiKey secretKey:(NSString *)secretKey)
-RCT_EXTERN_METHOD(loginUser:(NSString *)userId)
+// RCT_EXTERN_METHOD(loginUser:(NSString *)userId)
+RCT_EXTERN_METHOD(loginUser:(NSString *)userId
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(showOffers:(NSInteger)launchMode
                   route:(NSString)route
                   campaignID:(NSInteger)campaignID)
