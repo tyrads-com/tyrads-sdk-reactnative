8f1e159736dbe00b8f9ebd4f4929c9fe618da5fb
basharatmehdi <basharatmehdidar@gmail.com>
2025-04-15 13:31:24 +0530

    feat: loginUser changes to return data

--- ios/Tyrads/Tyrads.swift
+++ ios/Tyrads/Tyrads.swift
@@ -14,6 +14,7 @@ public class Tyrads {
     private var apiKey: String = ""
     private var apiSecret: String = ""
     private var publisherUserID: String = ""
+    private var currentLanguage: String = "en"
     private var newUser: Bool = false
     private var loginData: AcmoInitModel?
     var initializationWait = DispatchSemaphore(value: 0)
@@ -34,41 +35,19 @@ public class Tyrads {
         self.apiKey = apiKey
         self.apiSecret = secretKey
         self.debugMode = debugMode
+        self.currentLanguage = Locale.current.languageCode ?? ""
     }
 
     /// Logs in the user with the provided user ID or retrieves the user ID from UserDefaults.
     ///
     /// - Parameter userID: Optional. The user ID to log in with. If nil, the SDK will attempt to retrieve the user ID from UserDefaults.
-    @objc public func loginUser(_ userID: String? = nil) {
-        do {
-            let userId = userID ?? UserDefaults.standard.string(forKey: "acmo-tyrads-sdk-user-id") ?? ""
+    public func loginUser(_ userID: String? = nil, completion: @escaping (ApiHeaders?) -> Void) {
+        let userId = userID ?? UserDefaults.standard.string(forKey: "acmo-tyrads-sdk-user-id") ?? ""
 
-            let identifierType = "IDFA"
-            var advertisingId = ""
-            if #available(iOS 14, *) {
-                log("Requesting tracking authorization for iOS 14+")
-                ATTrackingManager.requestTrackingAuthorization { status in
-                    switch status {
-                    case .authorized:
-                        advertisingId = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-                        self.log("Tracking authorized. Advertising ID: \(advertisingId)")
-                    case .denied:
-                        advertisingId = ""
-                        self.log("Tracking denied")
-                    case .restricted:
-                        advertisingId = ""
-                        self.log("Tracking restricted")
-                    case .notDetermined:
-                        advertisingId = ""
-                        self.log("Tracking not determined")
-                    @unknown default:
-                        self.log("Unknown tracking status")
-                    }
-                }
-            } else {
-                advertisingId = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-                log("iOS version < 14. Advertising ID: \(advertisingId)")
-            }
+        let identifierType = "IDFA"
+        var advertisingId = ""
+
+        func finalizeLogin() {
             let fd: [String: Any] = [
                 "publisherUserId": userId,
                 "platform": "iOS",
@@ -76,10 +55,11 @@ public class Tyrads {
                 "identifier": advertisingId
             ]
 
-            log("Initializing with data: \(fd)")
+            self.log("Initializing with data: \(fd)")
 
             guard let url = URL(string: AcmoConfig.BASE_URL + "initialize") else {
-                log("Failed to create URL")
+                self.log("Failed to create URL")
+                completion(nil)
                 return
             }
 
@@ -94,47 +74,81 @@ public class Tyrads {
             do {
                 request.httpBody = try JSONSerialization.data(withJSONObject: fd)
             } catch {
-                log("Failed to serialize request body: \(error)")
+                self.log("Failed to serialize request body: \(error)")
+                completion(nil)
                 return
             }
 
             let task = URLSession.shared.dataTask(with: request) { data, response, error in
-
-            if let error = error {
-                self.log("Network request failed: \(error)")
-                return
-            }
-
-            guard let data = data else {
-                self.log("No data received from the server")
-                return
-            }
-
-            if let responseString = String(data: data, encoding: .utf8) {
-                self.log("Received response: \(responseString)")
-
-                let jsonData = responseString.data(using: .utf8)!
-                let decoder = JSONDecoder()
-                guard let acmoInitModel = try? decoder.decode(AcmoInitModel.self, from: jsonData) else {
-                    self.log("Failed to decode response")
+                if let error = error {
+                    self.log("Network request failed: \(error)")
+                    completion(nil)
                     return
                 }
-                self.loginData = acmoInitModel
-                self.publisherUserID = self.loginData?.data.user.publisherUserId ?? ""
-                self.newUser = self.loginData?.data.newRegisteredUser ?? false
-                self.log("Login successful. Publisher User ID: \(self.publisherUserID), New User: \(self.newUser)")
-                self.initializationWait.signal()
-            }
 
+                guard let data = data else {
+                    self.log("No data received from the server")
+                    completion(nil)
+                    return
+                }
 
+                if let responseString = String(data: data, encoding: .utf8) {
+                    self.log("Received response: \(responseString)")
+
+                    guard let jsonData = responseString.data(using: .utf8),
+                          let acmoInitModel = try? JSONDecoder().decode(AcmoInitModel.self, from: jsonData) else {
+                        self.log("Failed to decode response")
+                        completion(nil)
+                        return
+                    }
+
+                    self.loginData = acmoInitModel
+                    self.publisherUserID = acmoInitModel.data.user.publisherUserId
+                    self.newUser = acmoInitModel.data.newRegisteredUser
+                    self.log("Login successful. Publisher User ID: \(self.publisherUserID), New User: \(self.newUser)")
+                    self.initializationWait.signal()
+
+                    // Build ApiHeaders object
+                    let headers = ApiHeaders(
+                        xApiKey: self.apiKey,
+                        xApiSecret: self.apiSecret,
+                        xUserId: self.publisherUserID,
+                        xSdkPlatform: AcmoConfig.SDK_PLATFORM,
+                        xSdkVersion: AcmoConfig.SDK_VERSION,
+                        userAgent: UIDevice.current.systemName + "/" + UIDevice.current.systemVersion,
+                        languageCode: Locale.current.languageCode ?? "en",
+                        premiumColor: acmoInitModel.data.publisherApp.premiumColor,
+                        headerColor: acmoInitModel.data.publisherApp.headerColor,
+                        mainColor: acmoInitModel.data.publisherApp.mainColor
+                    )
+
+                    completion(headers)
+                }
             }
 
             task.resume()
-            log("Network request started")
+            self.log("Network request started")
+        }
 
-
-        } catch {
-            log("An error occurred: \(error)")
+        if #available(iOS 14, *) {
+            self.log("Requesting tracking authorization for iOS 14+")
+            ATTrackingManager.requestTrackingAuthorization { status in
+                switch status {
+                case .authorized:
+                    advertisingId = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+                    self.log("Tracking authorized. Advertising ID: \(advertisingId)")
+                case .denied, .restricted, .notDetermined:
+                    advertisingId = ""
+                    self.log("Tracking not authorized or restricted")
+                @unknown default:
+                    self.log("Unknown tracking status")
+                }
+                finalizeLogin()
+            }
+        } else {
+            advertisingId = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+            self.log("iOS version < 14. Advertising ID: \(advertisingId)")
+            finalizeLogin()
         }
     }
 
@@ -143,11 +157,18 @@ public class Tyrads {
 
      public func showOffers(_ launchMode: Int = 3, route: String? = nil, campaignID: Int? = nil) {
         self.initializationWait.wait()
-        var urlString =
-        "https://websdk.tyrads.com/?apiKey=\(Tyrads.instance.apiKey)&apiSecret=\(Tyrads.instance.apiSecret)&userID=\(Tyrads.instance.publisherUserID)&newUser=\(Tyrads.instance.newUser)&platform=\(AcmoConfig.SDK_PLATFORM)&hc=\(Tyrads.instance.loginData?.data.publisherApp.headerColor ?? "")&mc=\(Tyrads.instance.loginData?.data.publisherApp.mainColor ?? "")&launchMode=\(launchMode)&route=\(route ?? "")&campaignID=\(campaignID)"
+        var campaignIDString: String = "0"
+        if let campaignIDValue = campaignID {
+            campaignIDString = String(campaignIDValue)
+        }
+       let urlString =
+       "https://websdk.tyrads.com/?apiKey=\(Tyrads.instance.apiKey)&apiSecret=\(Tyrads.instance.apiSecret)&userID=\(Tyrads.instance.publisherUserID)&newUser=\(Tyrads.instance.newUser)&platform=\(AcmoConfig.SDK_PLATFORM)&hc=\(Tyrads.instance.loginData?.data.publisherApp.headerColor ?? "")&mc=\(Tyrads.instance.loginData?.data.publisherApp.mainColor ?? "")&launchMode=\(launchMode)&route=\(route ?? "")&campaignID=\(campaignIDString)&lang=\(Tyrads.instance.currentLanguage)&av=\(AcmoConfig.ACMO_VERSION)"
 
+        do {
+            guard let url = URL(string: urlString) else {
+                throw NSError(domain: "TyradsSdk", code: 1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+            }
 
-        if let url = URL(string: urlString) {
             switch launchMode {
             case 1, 2:
                 DispatchQueue.main.async {
@@ -171,6 +192,8 @@ public class Tyrads {
                     UIApplication.shared.open(url, options: [:], completionHandler: nil)
                 }
             }
+        } catch {
+            print("An error occurred: \(error)")
         }
     }
 }
