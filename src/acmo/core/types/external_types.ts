f6e17e037c03c1d5d3794a0e11b705997edb3671
basharatmehdi <basharatmehdidar@gmail.com>
2025-11-21 15:45:05 +0530

    created types for media source in ts

---
+++ src/acmo/core/types/external_types.ts
@@ -0,0 +1,22 @@
+export interface TyradsMediaSourceInfo {
+  mediaSourceName?: string,
+  mediaCampaignName?: string,
+  mediaSourceId?: string,
+  mediaSubSourceId?: string,
+  incentivized?: boolean,
+  mediaAdsetName?: string,
+  mediaAdsetId?: string,
+  mediaCreativeName?: string,
+  mediaCreativeId?: string,
+  sub1?: string,
+  sub2?: string,
+  sub3?: string,
+  sub4?: string,
+  sub5?: string,
+}
+
+export interface TyradsUserInfo {
+  email?: string,
+  phoneNumber?: string,
+  userGroup?: string
+}
