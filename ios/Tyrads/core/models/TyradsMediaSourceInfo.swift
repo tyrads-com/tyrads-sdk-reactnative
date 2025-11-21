82a827448abd216e7d9f07ad397a6885f2a38a38
basharatmehdi <basharatmehdidar@gmail.com>
2025-11-21 15:47:10 +0530

    created models for user info and media source

---
+++ ios/Tyrads/core/models/TyradsMediaSourceInfo.swift
@@ -0,0 +1,25 @@
+//
+//  TyradsMediaSourceInfo.swift
+//  Pods
+//
+//  Created by Basharat Mehdi on 21/11/25.
+//
+
+import Foundation
+
+public struct TyradsMediaSourceInfo: Codable {
+  public var mediaSourceName: String?
+  public var mediaCampaignName: String?
+  public var mediaSourceId: String?
+  public var mediaSubSourceId: String?
+  public var incentivized: Bool?
+  public var mediaAdsetName: String?
+  public var mediaAdsetId: String?
+  public var mediaCreativeName: String?
+  public var mediaCreativeId: String?
+  public var sub1: String?
+  public var sub2: String?
+  public var sub3: String?
+  public var sub4: String?
+  public var sub5: String?
+}
---
+++ ios/Tyrads/core/models/TyradsUserInfo.swift
@@ -0,0 +1,14 @@
+//
+//  TyradsUserInfo.swift
+//  Pods
+//
+//  Created by Basharat Mehdi on 21/11/25.
+//
+
+import Foundation
+
+public struct TyradsUserInfo: Codable {
+  var email: String?
+  var phoneNumber: String?
+  var userGroup: String?
+}
