accb33de6b407bfc5de6ce7fe72c694a582a7fd3
basharatmehdi <basharatmehdidar@gmail.com>
2026-01-17 17:23:22 +0530

    remove manual push notification boilerplate

--- example/ios/TyradsSdkExample/AppDelegate.mm
+++ example/ios/TyradsSdkExample/AppDelegate.mm
@@ -2,47 +2,30 @@
 
 #import <React/RCTBundleURLProvider.h>
 
-#import <tyrads_sdk/tyrads_sdk-Swift.h>
-
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   self.moduleName = @"TyradsSdkExample";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  [APNsAppDelegateProxy configure];
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  return [super application:application
+      didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (void)application:(UIApplication *)application
-didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-  [APNsAppDelegateProxy didRegisterForRemoteNotifications:deviceToken];
-}
-
-- (void)application:(UIApplication *)application
-didReceiveRemoteNotification:(NSDictionary *)userInfo
-fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
-{
-  [APNsAppDelegateProxy didReceiveRemoteNotification:userInfo
-                                  completionHandler:completionHandler];
-}
-
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
   return [self bundleURL];
 }
 
-- (NSURL *)bundleURL
-{
+- (NSURL *)bundleURL {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  return
+      [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [[NSBundle mainBundle] URLForResource:@"main"
+                                 withExtension:@"jsbundle"];
 #endif
 }
 
