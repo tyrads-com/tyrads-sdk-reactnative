2b8b21c8c3a46f7074a7ddb41554d892d02275d6
basharatmehdi <basharatmehdidar@gmail.com>
2026-01-20 14:22:21 +0530

    implemented and exposed preload offers native module

--- ios/TyradsSdk.swift
+++ ios/TyradsSdk.swift
@@ -91,6 +91,14 @@ class TyradsSdk: RCTEventEmitter {
       resolve(nil)
     }
   }
+
+  @objc(preloadOffers:resolver:rejecter:)
+  func preloadOffers(_ route: String?, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    Task { @MainActor in
+      Tyrads.instance.preloadOffers(route: route)
+      resolve(nil)
+    }
+  }
   
   @objc(changeLanguage:resolver:rejecter:)
   func changeLanguage(_ lang: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
