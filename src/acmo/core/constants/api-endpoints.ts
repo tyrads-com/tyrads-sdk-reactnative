e33b201e919640299dd8f477b7744ae940cb45f6
basharatmehdi <basharatmehdidar@gmail.com>
2026-03-26 14:10:57 +0530

    sync API endpoint constants with latest versioning

--- src/acmo/core/constants/api-endpoints.ts
+++ src/acmo/core/constants/api-endpoints.ts
@@ -1,9 +1,10 @@
 class AcmoAPIEndpoints {
   public static readonly TARGETED_CAMPAIGNS = "campaigns";
-  public static readonly ACTIVE_CAMPAIGNS = "activated-campaigns";
-  public static readonly ENGAGEMENT = "engagement";
-  public static readonly USER_ACTIVITIES = "user-activities";
-  public static readonly ACTIVATE_CAMPAIGN = (id: number) => `campaigns/active/${id}`;
+  public static readonly ACTIVE_CAMPAIGNS = "campaigns/activated";
+  public static readonly SUMMARY = "campaigns/activated/summary";
+  public static readonly ENGAGEMENT = "account/engagement";
+  public static readonly USER_ACTIVITIES = "account/activity";
+  public static readonly ACTIVATE_CAMPAIGN = (id: number) => `campaigns/${id}/activate`;
   public static readonly TRANSLATIONS = (locale: string) => `translations/${locale}`;
   public static readonly TRANSLATIONS_VERSION = "translations/version";
 }
