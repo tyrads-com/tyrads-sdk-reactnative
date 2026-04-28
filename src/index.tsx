d898e3fda48c03eb4566d02b508f655e0076264d
basharatmehdi <basharatmehdidar@gmail.com>
2026-04-28 10:08:50 +0530

    refactor premium widget exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
 import { AppRegistry } from 'react-native';
 import TyradsSdkCore from './acmo/core/tyrads-sdk-core';
 import TyradsGlobalHost from './acmo/modules/inapp-notifications/tyrads-global-host';
-import { PremiumWidgetStyles } from './acmo/modules/premium-widgets/premium_offers_widget';
-import PremiumWidgetsLoading from './acmo/modules/premium-widgets/components/premium_loading';
 import type { TyradsConfig, TyradsMediaSourceInfo, TyradsUserInfo } from './acmo/core/types/external_types'
-import BasePremiumOffersWidget from './acmo/modules/premium-widgets/premium_offers_widget';
-import { LocalizationProvider } from './acmo/modules/localization/localization_context';
-import { View } from 'react-native';
 
 AppRegistry.setWrapperComponentProvider(() => (props) => (
   <TyradsGlobalHost {...props} />
@@ -14,40 +9,7 @@ AppRegistry.setWrapperComponentProvider(() => (props) => (
 
 export type { TyradsConfig, TyradsMediaSourceInfo, TyradsUserInfo } from './acmo/core/types/external_types'
 export { PremiumWidgetStyles } from './acmo/modules/premium-widgets/premium_offers_widget';
-
-export const PremiumOffersWidget = ({
-  widgetStyle = PremiumWidgetStyles.list,
-  launchMode = 2,
-}: {
-  widgetStyle?: PremiumWidgetStyles;
-  launchMode?: number;
-}) => {
-  const handleNavigation = (route?: string, campaignID?: number | null) => {
-    TyradsSdkCore.showOffers({ route: route, campaignID: campaignID, launchMode: launchMode });
-  };
-  return (
-    <LocalizationProvider>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <BasePremiumOffersWidget
-          widgetStyle={widgetStyle}
-          onNavigate={handleNavigation}
-        />
-      </View>
-    </LocalizationProvider>
-  );
-};
-
-export const PremiumOffersWidgetLoading = (
-  { widgetStyle = PremiumWidgetStyles.list }: {
-    widgetStyle?: PremiumWidgetStyles;
-  }
-) => {
-  return (
-    <PremiumWidgetsLoading
-      widgetStyle={widgetStyle}
-    />
-  );
-};
+export { PremiumOffersWidget, PremiumOffersWidgetLoading } from './acmo/modules/premium-widgets/premium_offers_widget_wrapper';
 
 const Tyrads = {
   init: async (apiKey: string, apiSecret: string, encKey?: string, engagementId?: string, placementId?: string, mediaSourceInfo?: TyradsMediaSourceInfo, userInfo?: TyradsUserInfo, config?: TyradsConfig) => {
